ea23ad93c613a51181b23accf593fdd41a633ec5
GabrielMihai <gabriel04mihai89@gmail.com>
2024-05-26 07:20:11 +0100

    features added : product and category models , installed pillow

--- ecommerceproject/shop/models.py
+++ ecommerceproject/shop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=50, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name

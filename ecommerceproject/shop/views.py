e63d6b537c45b5f688714b217fb33163bf0a69a3
GabrielMihai <gabriel04mihai89@gmail.com>
2024-05-26 09:09:37 +0100

    create models,views,urls,forms,modified settings in main folder

--- ecommerceproject/shop/views.py
+++ ecommerceproject/shop/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
-# ceva de la mine
+from django.views.generic import CreateView, ListView
+
+from ecommerceproject.shop.forms import CategoryForm
+from ecommerceproject.shop.models import Category
+
+
+class CategoryCreateView(CreateView):
+    template_name = 'shop/create_category.html'
+    model = Category
+    form_class = CategoryForm
+    success_url = reverse_lazy('list_categories')
+
+
+class CategoryListView(ListView):
+    template_name = 'shop/category_list.html'
+    model = Category
+    context_object_name = 'categories'
+

c35c260859f1334187e232ce8b4937ef502a57fa
GabrielMihai <gabriel04mihai89@gmail.com>
2024-05-27 22:19:55 +0100

    delete the product app and modified the files from ecommerce project and from shop app

--- ecommerceproject/shop/views.py
+++ ecommerceproject/shop/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, TemplateView
 
 from .forms import CategoryForm
-from .models import Category
+from .models import Category, Product
 
 
 class CategoryCreateView(CreateView):
@@ -22,3 +22,15 @@ class CategoryListView(ListView):
 class HomeView(TemplateView):
     template_name = 'homepage.html'
 
+
+class CreateProductView(CreateView):
+    template_name = 'product/create_product.html'
+    model = Product
+    # form_class = ProductForm
+    success_url = reverse_lazy('list_categories')
+
+
+class ListProductView(ListView):
+    template_name = 'product/list_products.html'
+    model = Product
+    context_object_name = 'products'

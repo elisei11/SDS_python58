4b322fb6228dfa045257fb7e00f36e714cea08fa
GabrielMihai <gabriel04mihai89@gmail.com>
2024-06-19 22:50:33 +0100

    fixed forms for reviews , login,sign_up,place_order,carousel edges on round ,

--- feedback/forms.py
+++ feedback/forms.py
@@ -3,10 +3,16 @@ from django import forms
 from feedback.models import Feedback
 
 
+from django import forms
+from .models import Feedback
+
 class FeedbackForm(forms.ModelForm):
+    RATING_CHOICES = [(i, str(i)) for i in range(1, 6)]  # Opțiuni valide: 1, 2, 3, 4, 5
+    rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Feedback
         fields = ['comment', 'rating']
-        # widgets = {
-        #     'product': forms
-        # }
+        widgets = {
+            'comment': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Comment'}),
+        }
